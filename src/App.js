9c01e0b2b040596b8f16209a7a3c4bf4266f5623
Kelvin Chan <kchan@securitycompass.com>
2019-09-07 17:37:21 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import GuestTable from './GuestTable';
+import Grid from '@material-ui/core/Grid';
+import { ReactComponent as FlightIcon } from './flight-icon.svg';
+import { createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+
+import Papa from 'papaparse';
+
+const guestList = [];
+
+function loadGuests() {
+  return fetch('/guests.csv').then(function (response) {
+    let reader = response.body.getReader();
+    let decoder = new TextDecoder('utf-8');
+
+    return reader.read().then(function (result) {
+        return decoder.decode(result.value);
+    });
+  });
+}
+
+
+function createData(lastName, firstName, from, destination) {
+  return { lastName, firstName, from, destination };
+}
+
+function Header() {
+  return (
+    <div style={{
+      margin: 12,
+      paddingLeft: 50,
+    }}>
+      <FlightIcon fill="white" width={55} height={55} style={{
+        top: ".47em",
+        position: "relative"
+      }} />
+      <Typography variant="h2" component="body1" align="left" style={{paddingLeft: 20}}>
+        {'ARRIVALS'}
+      </Typography>
+    </div>
+
+  );
+}
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      guestList: [],
+      page: 0,
+      rowsPerPage: 42
+    }
+  };
+
+  async loadData() {
+    const csvData = await loadGuests();
+
+    Papa.parse(csvData, {
+        complete: (result) => {
+          const guestList = [];
+          result.data.forEach(guest => {
+            guestList.push(createData(guest[2], guest[3], guest[7], guest[9]))
+          });
+          this.setState({ guestList })
+        }
+    });
+  };
+
+  componentWillMount() {
+    this.loadData();
+    setInterval(() => {
+      const maxPage = Math.ceil(this.state.guestList.length / this.state.rowsPerPage);
+
+      this.setState({ page: (this.state.page + 1) % maxPage })
+    }, 1000);
+  }
+
+  render() {
+    const theme = createMuiTheme({
+      palette: {
+        type: 'dark',
+      },
+    });
+
+    return (
+      <ThemeProvider theme={theme}>
+        <div>
+          <CssBaseline />
+
+          <Header />
+
+          <Grid container spacing={0}>
+            <Grid item xs>
+              <GuestTable rows={this.state.guestList} rowsPP={this.state.rowsPerPage} pageNum={this.state.page} />
+            </Grid>
+          </Grid>
+        </div>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/GuestTable.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 650,
+  },
+  flatCell: {
+    padding: "2px 20px 2px 16px"
+  },
+}));
+
+
+function getFlag(location) {
+  if (location == "Toronto") {
+    return "🇨🇦";
+  } else if (location == "Hong Kong") {
+    return "🇭🇰";
+  } else if (location == "Sydney") {
+    return "🇦🇺";
+  } else if (location == "New York" || location == "San francisco") {
+    return "🇺🇸";
+  } else if (location == "Kota Kinabalu") {
+    return "🇲🇾";
+  } else if (location == "Paris") {
+    return "🇫🇷";
+  }
+}
+export default function GuestTable({ rows = [], initialPage = 0, rowsPP = 24, pageNum = 0 }) {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(initialPage);
+  const [rowsPerPage, setRowsPerPage] = React.useState(rowsPP);
+
+  return (
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow style={{paddingLeft: 20}}>
+            <TableCell style={{paddingLeft: 70}} width={150} className={classes.flatCell}>Time</TableCell>
+            <TableCell width={220} className={classes.flatCell}>Passenger</TableCell>
+            <TableCell width={220} className={classes.flatCell}></TableCell>
+            <TableCell width={150} className={classes.flatCell}>Flight</TableCell>
+            <TableCell width={25} style={{ paddingRight: 0 }} className={classes.flatCell}></TableCell>
+            <TableCell width={200} className={classes.flatCell}>From</TableCell>
+            <TableCell className={classes.flatCell}>Destination</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+            rows.slice(pageNum * rowsPerPage, pageNum * rowsPerPage + rowsPerPage).map(
+              row => {
+                return (
+                  <TableRow style={{paddingLeft: 20}} key={row.name}>
+                    <TableCell style={{ paddingLeft: 70, fontWeight: "bold" }} className={classes.flatCell}>{row.lastName && "7:00PM" || ""}</TableCell>
+                    <TableCell className={classes.flatCell}>{row.lastName}</TableCell>
+                    <TableCell className={classes.flatCell}>{row.firstName}</TableCell>
+                    <TableCell style={{ fontWeight: "bold", color: "rgb(255,192,0)" }} className={classes.flatCell}>{row.lastName && "KK 0908" }</TableCell>
+                    <TableCell style={{ paddingRight: 0 }} className={classes.flatCell}>{row.from && getFlag(row.from) }</TableCell>
+                    <TableCell style={{ fontWeight: "bold" }} className={classes.flatCell}>{row.from}</TableCell>
+                    <TableCell className={classes.flatCell}>{row.destination}</TableCell>
+                  </TableRow>
+                );
+            })
+          }
+        </TableBody>
+      </Table>
+  );
+};
